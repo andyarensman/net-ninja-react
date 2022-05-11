870cb043dcf28febb2209a5cea9c5724f0dff8e2
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-05-10 18:47:20 -0700

    lessons 16 & 17 - Using JSON Server % Fetching Data with useEffect

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Reviewing more React with the Net Ninja YouTube Channel.
+
+To start the json server, use the following command:
+
+    npx json-server --watch data/db.json --port 8000
+
+## Conditionally Output Data
+
+In order to avoid a component loading before the data has loaded, use a conditional like this:
+
+    {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
--- src/Home.js
+++ src/Home.js
@@ -3,30 +3,25 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
-
-  const [name, setName] = useState('Mario');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
     console.log('use effect ran');
-    console.log(name)
-  }, [name]) //only runs the function once/only initial render when the second argument is an empty array - []
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json()
+      })
+      .then((data) => {
+        setBlogs(data);
+      })
+  }, []) //only runs the function once/only initial render when the second argument is an empty array - []
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-      <button onClick={() => setName('Luigi')}>change name</button>
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {/*<button onClick={() => setName('Luigi')}>change name</button>
       <p>{name}</p>
-      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" handleDelete={handleDelete}/> */}
+       <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" handleDelete={handleDelete}/> */}
     </div>
   );
 }
