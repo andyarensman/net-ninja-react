b47bd5ddcafbbfdc705b71466a554d3a83bc9e57
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-05-11 11:25:03 -0700

    lessons 18, 19, 20 - loading messages, fetch errors, custom hooks

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ To start the json server, use the following command:
 
     npx json-server --watch data/db.json --port 8000
 
+## useEffect
+
+    useEffect(() => {}, []) //only runs the function once/only initial render when the second argument is an empty array - []
+
 ## Conditionally Output Data
 
 In order to avoid a component loading before the data has loaded, use a conditional like this:
--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,14 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-
-  const [blogs, setBlogs] = useState(null);
-
-  useEffect(() => {
-    console.log('use effect ran');
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then((data) => {
-        setBlogs(data);
-      })
-  }, []) //only runs the function once/only initial render when the second argument is an empty array - []
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
+      {error && <div> {error} </div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       {/*<button onClick={() => setName('Luigi')}>change name</button>
       <p>{name}</p>
