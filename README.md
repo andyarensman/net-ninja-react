ec4d6844dfc662b3139e85b29bc22f3202a7a06d
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-05-13 12:13:41 -0700

    lessons 28, 29, 30, 31, 32 - Submit Events, Post Request, Programmatic Redirects, Deleting, 404 page

--- README.md
+++ README.md
@@ -19,3 +19,7 @@ In order to avoid a component loading before the data has loaded, use a conditio
 He uses version 5 in the tutorial, however this version does not work with the newest version of React, so I had to update to version 6 and change a few things in the code. 'Switch' is now 'Routes'.
 
 https://github.com/remix-run/react-router/blob/main/docs/getting-started/tutorial.md
+
+In version 6, useHistory is replaced by useNavigate. [See this documentation for more info on the differences](https://reactrouter.com/docs/en/v6/upgrading/v5).
+
+To go back by one page, you use `navigate(-1)` instead of `history.go(-1)`.
--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,36 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author }; //don't need id because json serve does it for us
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added')
+      setIsPending(false);
+      // navigate(-1)
+      navigate('/');
+    })
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
         <input
           type="text"
@@ -30,10 +52,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        { !isPending && <button>Add Blog</button> }
+        { isPending && <button disabled>Adding Blog...</button> }
       </form>
     </div>
   );
